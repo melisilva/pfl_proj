a369c3c8c83b10fe67dcc10d900f6a1f39f5ee4a
melisilva <melissa.silva2001.ms@gmail.com>
2021-11-11 15:19:42 +0000

    Corrected fibRec + added other fib definitions

--- Fib.hs
+++ Fib.hs
@@ -3,5 +3,13 @@ fibRec :: (Integral a) => a -> a
 --fibRec tem dois casos base: quando a toma o valor de 0 e o valor de 1
 fibRec 0 = 0
 fibRec 1 = 1
-fibRec a | n > 0 = fibRec (n-2) + fibRec (n-1)
+fibRec a | a > 0 = fibRec (a-2) + fibRec (a-1)
          | otherwise = error "Número negativo" --professor disse para não usar error, devemos usar o quê então?
+
+--1.2) Função de fibonacci programação dinâmica
+fibLista :: (Integral a) => a -> a
+
+
+
+--1.3) Função de fibonacci lista infinita
+fibListaInfinita :: (Integral a) => a -> a
