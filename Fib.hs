ead7950abfe3c1b1f01f707997a1300188236b48
melisilva <melissa.silva2001.ms@gmail.com>
2021-11-11 16:22:05 +0000

    1.2 thoughts

--- Fib.hs
+++ Fib.hs
@@ -7,7 +7,10 @@ fibRec a | a > 0 = fibRec (a-2) + fibRec (a-1)
          | otherwise = error "Número negativo" --professor disse para não usar error, devemos usar o quê então?
 
 --1.2) Função de fibonacci programação dinâmica
-fibLista :: [Integer]
+--Penso que como argumento devemos dar um integer e calcular a sequência de fibonacci até esse Número
+--ex n=10-->[0,1,1,2,3,5,8,13,21,34,55]
+--if you do [0,1,1,2,3,5,8,13,21,34,55] !! 10, you get 10-->as it's asked
+fibLista :: (Integral a) => a -> [Integer]
 
 --1.3) Função de fibonacci lista infinita
 fibListaInfinita :: [Integer]
