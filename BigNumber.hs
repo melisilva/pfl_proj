031272296ee1272558774a338e4852118eceeaf3
Mateus Silva <42583814+lessthelonely@users.noreply.github.com>
2021-11-12 10:32:41 +0000

    ok starting ops again

--- BigNumber.hs
+++ BigNumber.hs
@@ -83,25 +83,3 @@ unidades x = ["um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "
 
 offsets x = ["onze", "doze", "treze", "catorze", "quinze", "dezasseis", "dezassete", "dezoito", "dezanove"]!!(x-1)
 
--- Função útil para operações.
-algarismos :: Int -> BigNumber
-algarismos 0 = []
-algarismos n = if n<0 then conv(algarismos(div (n*(-1)) 10) ++ [mod (n*(-1)) 10]) else algarismos (div n 10) ++ [mod n 10]
-
---2.4) Função somaBN
-somaBN :: BigNumber -> BigNumber -> BigNumber
-somaBN a b = algarismos(fromDigits(a)+fromDigits(b))
-
---2.5) Função subBN
-subBN :: BigNumber -> BigNumber -> BigNumber
-subBN a b = algarismos(fromDigits(a)-fromDigits(b))
-
---2.6) Função mulBN
-mulBN :: BigNumber -> BigNumber -> BigNumber
-mulBN a b = algarismos(fromDigits(a)*fromDigits(b))
-
--- 2.7) Função divBN
-divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
-divBN a b = (algarismos x, algarismos y)
-      where x = div (fromDigits a) (fromDigits b)
-            y = mod (fromDigits a) (fromDigits b)
