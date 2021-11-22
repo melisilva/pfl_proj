0b1b0910a116183d3b583b215069becd227d0793
Mateus Silva <42583814+lessthelonely@users.noreply.github.com>
2021-11-22 22:32:11 +0000

    added comments + code cleaning
    found 2 functions no one used.

--- BigNumber.hs
+++ BigNumber.hs
@@ -8,6 +8,13 @@ import Data.String
 type BigNumber = [Int]
 
 -- Operadores de comparação
+-- Corresponde ao operador <.
+-- Compara dois valores BigNumber. Não retorna True se forem iguais.
+-- Tem em conta operandos vazios, considerando-os iguais a zero.
+-- E operandos com zeros desnecessários à esquerda, adicionados em contexto de operações.
+-- Se nenhum dos anteriores se verificar, compara número de algarismos.
+-- E, nos últimos três casos, compara os números algarismo a algarismo.
+-- O primeiro par de algarismos que não seja igual ditará o resultado.
 less :: BigNumber -> BigNumber -> Bool
 less a b
       | a == b                    = False
@@ -21,10 +28,15 @@ less a b
       | (take 1 b) == [0]         = less a (tail b)
       | length a > length b       = False
       | length a < length b       = True
-      | (head a) == (head b)      = (<) (tail a) (tail b)
+      | (head a) == (head b)      = less (tail a) (tail b)
       | (head a) < (head b)       = True
       | (head a) > (head b)       = False
 
+
+-- Diz se dois valores BigNumber são iguais.
+-- Tem conta operandos com zeros à esquerda.
+-- Compara comprimentos dos operados.
+-- Utiliza a função any para uma comparação mais eficiente.
 equal :: BigNumber -> BigNumber -> Bool
 equal a b
       | a == b                    = True
@@ -35,12 +47,17 @@ equal a b
       | otherwise                                                                = False
 
 -- 2.2) Função scanner
+-- Transforma um número inteiro em BigNumber, algarismo a algarismo.
+-- Utiliza quot e rem para tal. 
 toBN :: Int -> BigNumber
 toBN 0 = [0]
 toBN n
       | abs n >= 10    =  toBN (quot n 10) ++ [rem n 10]
       | otherwise      = [rem n 10]
 
+-- Transforma uma String em BigNumber.
+-- Fá-lo transformando a String em Int e daí usa toBN para passar de Int a BigNumber.
+-- Tem em conta Strings com números negativos.
 scanner :: String -> BigNumber
 scanner str
       | head str == '-'            = (head treatAsPosRes: map (*(-1)) (tail treatAsPosRes))
@@ -49,43 +66,45 @@ scanner str
        num = read str :: Int
 
 -- 2.3) Função output
-fromBN :: BigNumber -> Int
-fromBN xs = if head(xs) < 0 then (-1)*(sum (zipWith (*) (reverse (conv xs)) (iterate (*10) 1))) else sum (zipWith (*) (reverse xs) (iterate (*10) 1))
-
-conv :: BigNumber -> BigNumber
-conv xs = ((-1)*head(xs)): drop 1 xs
-
+-- Transforma uma lista com Strings de algarismos numa String de algarismos.
 strinNum :: [String] -> String
 strinNum xs = if length(xs) /= 1 then head(xs) ++ strinNum(drop 1 xs) else head(xs)
 
+-- Transforma um BigNumber em String (usa strinNum).
+-- Faz o oposto de scanner.
 output :: BigNumber -> String
 output xs = strinNum (map show xs)
 
 -- utils
+-- Converte todos os algarismos de um BigNumber negativo em algarismos negativos.
+-- Utilizada em somas e subtrações, sobretudo.
 utilNegative :: BigNumber -> BigNumber
 utilNegative a
       | (head a) < 0                  = (head a) : [ (a !! x)*(-1) | x<-[1..len]]
       | otherwise                     = a
   where len = length a - 1
 
+-- Adiciona zeros à esquerda de xs até este ter comprimento n.
+-- Usada para alinhar corretamente operandos em somas e subtrações.
 utilPad :: Int -> BigNumber -> BigNumber
 utilPad n xs = replicate (n - length xs) 0 ++ xs
 
+-- Adiciona n zeros à direita de xs.
+-- Usada para alinhar corretamente operandos em somas no contexto de multiplicações.
 utilPadR :: Int -> BigNumber -> BigNumber
 utilPadR n xs = xs ++ replicate n 0
 
+-- Desfaz o que utilPad faz.
 utilUnpad :: BigNumber -> BigNumber
 utilUnpad (x:xs)
       | x == 0 && length (x:xs) /= 1   = utilUnpad (drop 1 (x:xs))
       | otherwise = (x:xs)
 
-toDigits :: Int -> BigNumber
-toDigits 0 = [0]
-toDigits n = if n<0 then conv(toDigits(div (n*(-1)) 10) ++ [mod (n*(-1)) 10]) else toDigits (div n 10) ++ [mod n 10]
-
-delZero :: BigNumber -> BigNumber
-delZero n = if (head(n)==0 && length(n)/=1) then drop 1 n else n
-
+-- Processa o resultado do zipWith feito em somaBN.
+-- Emana o processo de uma conta de soma "em pé": sempre que a soma dá
+-- mais que dez, "vai um" para o elemento seguinte da lista.
+-- Como a lista é dada em ordem contrária, ir para a esquerda (como na
+-- conta manual), é seguir para a direita na lista que é argumento desta função.
 utilSoma :: BigNumber -> BigNumber
 utilSoma [] = []
 utilSoma l
@@ -97,6 +116,10 @@ utilSoma l
         y = head (drop 1 l)
         xs = drop 2 l
 
+-- Processa o resultado do zipWith feito em subBN.
+-- Emana o processo de uma conta de subtração "em pé": sempre que ultrapassarmos
+-- os limites 0 ou 10, temos de SUBTRAIR 1 ao elemento seguinte da lista e continuamos.
+-- De outra forma, usamos mod e quot ou apenas mantemos o valor inicial na lista l e a conta termina.
 utilSub :: BigNumber -> BigNumber
 utilSub [] = []
 utilSub l
@@ -110,12 +133,20 @@ utilSub l
         y = head (drop 1 l)
         xs = drop 2 l
 
+-- Utilizada para procurar por resultados negativos numa lista resultante
+-- do zipWith de mulBN - se um valor for negativo, então o produto será
+-- também negativo.
 utilSig :: BigNumber -> BigNumber
 utilSig num
       | any (<0) num == True && head num > 0 = (head num)*(-1) : (map (abs) (tail num))
       | any (<0) num == True && head num < 0 = head num : map (abs) (tail num)
       | otherwise                            = num
 
+-- Utilizada para processar o resultado de zipWith de mulBN.
+-- A lógica é semelhante a utilSoma e utilSub, apenas se lidam só e apenas
+-- com quocientes (que podem ser somados ao elemento seguinte, em vez de um
+-- "vai 1", pode ser um "vai 3" ou "vai 4", etc...) e restos.
+-- De outra forma, é apenas retorna o resultado, caso este seja < 10 em módulo.
 processProduct :: BigNumber -> BigNumber
 processProduct [] = []
 processProduct l
@@ -128,7 +159,10 @@ processProduct l
   where x = head l
         y = head (drop 1 l)
         xs = drop 2 l
-
+      
+-- Utilizada para processar a soma de todos os subprodutos realizados
+-- em mulBN. Utiliza utilPadR para alinhar corretamente os subprodutos
+-- para a soma final.
 utilMul :: Int -> BigNumber -> BigNumber -> BigNumber
 utilMul i a b
       | length b > 1           = somaBN currentProduct (utilMul (i + 1) a next)
@@ -138,9 +172,12 @@ utilMul i a b
         x = last b
         next = init b
 
+-- Troca o sinal de um BigNumber negativo para positivo.
+-- De outra forma, mantém o sinal positivo.
 utilPositive :: BigNumber -> BigNumber
 utilPositive a = if head(a)<0 then mulBN [-1] a else a
 
+-- Determina o comprimento de uma janela de divisão válida para utilDiv.
 getLenA :: Int -> BigNumber -> BigNumber -> Int
 getLenA i a b
       | less a b                                  = 0
@@ -149,6 +186,9 @@ getLenA i a b
       | less (take i a) b                         = getLenA (i + 1) a b
       | otherwise                                 = i
 
+-- Processa a divisão como a operação manual.
+-- Lida com operandos vazios ou iguais a 0 e com divisor igual a 1.
+-- Ver explicação no README na explicação da Implementação de divBN.
 utilDiv :: BigNumber -> BigNumber -> BigNumber -> BigNumber
 utilDiv _ [] _ = []
 utilDiv _ _ [] = []
@@ -161,12 +201,12 @@ utilDiv i a b
       | less a b && take 1 a == zero && length a == 1                                             = []
       | take 1 a == zero                                                                          = zero ++ utilDiv zero (drop 1 initA) b
       | less quo initA && less prod initA && not (equal prod initA)                               = utilDiv (somaBN i one) a b
-      | less quo initA && equal prod initA && divRest == []                                   = somaBN i one
-      | less quo initA && equal prod initA && (take 1 divRest) == zero && divRest /= zero = somaBN i one ++ zero ++ utilDiv zero (divRest) b
-      | less quo initA && equal prod initA && (take 1 divRest) == zero && divRest == zero = somaBN i one ++ zero ++ utilDiv zero (divRest) b
-      | less quo initA && equal prod initA && divRest /= []                                   = somaBN i one ++ utilDiv zero (divRest) b
-      | less quo initA && not (less prod initA) && divRest /= [] && sub /= zero               = i ++ utilDiv zero (sub ++ divRest) b
-      | less quo initA && not (less prod initA) && divRest /= [] && sub == zero               = i ++ utilDiv zero divRest b
+      | less quo initA && equal prod initA && divRest == []                                       = somaBN i one
+      | less quo initA && equal prod initA && (take 1 divRest) == zero && divRest /= zero         = somaBN i one ++ zero ++ utilDiv zero (divRest) b
+      | less quo initA && equal prod initA && (take 1 divRest) == zero && divRest == zero         = somaBN i one ++ zero ++ utilDiv zero (divRest) b
+      | less quo initA && equal prod initA && divRest /= []                                       = somaBN i one ++ utilDiv zero (divRest) b
+      | less quo initA && not (less prod initA) && divRest /= [] && sub /= zero                   = i ++ utilDiv zero (sub ++ divRest) b
+      | less quo initA && not (less prod initA) && divRest /= [] && sub == zero                   = i ++ utilDiv zero divRest b
       | otherwise                                                                                 = i
   where initA = if (getLenA 1 a b) /= length a then take (getLenA 1 a b) a else a
         quo = mulBN i b
@@ -179,6 +219,9 @@ utilDiv i a b
         ten = one ++ zero
 
 -- 2.4 ) somaBN
+-- Efetua a soma de dois BigNumber. Tem em conta operandos negativos, fazendo chamada a subBN.
+-- Apenas soma, realmente, operandos positivos. Todos os outros casos são reduzidos a subtrações.
+-- Tem em conta o elemento neutro da adição.
 somaBN :: BigNumber -> BigNumber -> BigNumber
 somaBN [0] b = b
 somaBN a [0] = a
@@ -196,6 +239,9 @@ somaBN a b
         sumNeg = somaBN (((head a))*(-1):tail a) (((head b))*(-1):tail b)
 
 -- 2.5 ) subBN
+-- Efetua a subtração entre dois BigNumber.
+-- Subtrações que igualem somas são entregues a somaBN (por exemplo, quando b for negativo).
+-- Tem em conta o elemento neutro da subtração.
 subBN :: BigNumber -> BigNumber -> BigNumber
 subBN a [] = a
 subBN [] b = ((head b)*(-1):tail b)
@@ -216,6 +262,8 @@ subBN a b
         goesNegative = subBN b a
 
 -- 2.6) mulBN
+-- Efetua o produto entre dois BigNumber.
+-- Tem em conta números negativos e produtos pelos elementos absorvente e neutro da multiplicação.
 mulBN :: BigNumber -> BigNumber -> BigNumber
 mulBN [] b = [0]
 mulBN a [] = [0]
@@ -232,6 +280,10 @@ mulBN a b
         aPbN = utilMul 0 a (utilNegative b)
 
 -- 2.7) divBN
+-- Efetua a divisão entre dois BigNumber.
+-- Operandos negativos são considerados e lidados de forma adequada.
+-- Tem em conta números negativos e retorna resultados iguais a quot e rem.
+-- A divisão manual encontra-se em utilDiv, que só recebe operandos positivos ou [0], no caso do seu primeiro argumento..
 divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
 divBN [] _ = ([], [])
 divBN _ [] = error "EMPTY DIVISOR"
@@ -249,7 +301,19 @@ divBN a b
         one = [1]
         zero = [0]
 
+-- Alínea 4 (funções auxiliares)
+-- Faz o inverso de toBN, utilizando sum, zipWith, conv e iterate com produto por 10.
+fromBN :: BigNumber -> Int
+fromBN xs = if head(xs) < 0 then (-1)*(sum (zipWith (*) (reverse (conv xs)) (iterate (*10) 1))) else sum (zipWith (*) (reverse xs) (iterate (*10) 1))
+
+-- Inverte o sinal de um BigNumber.
+conv :: BigNumber -> BigNumber
+conv xs = ((-1)*head(xs)): drop 1 xs
+
 -- Alínea 5
+-- Usa a Monad Maybe para implementar uma divisão segura, que considera
+-- que o divisor fornecido pode ser zero. Nesse caso, retorna Nothing.
+-- De outra forma, retorna o resultado da divisão encapsulado em Just.
 safeDivBN :: BigNumber -> BigNumber -> Maybe (BigNumber, BigNumber)
 safeDivBN a b
       | b == [0]              = Nothing
--- Fib.hs
+++ Fib.hs
@@ -2,22 +2,31 @@ import BigNumber
 import GHC.Num
 
 -- Alínea 1
+-- Utiliza recursão direta para o cálculo de números de Fibonacci: fibRec(n) = fibRec(n-1) + fibRec(n - 2).
 fibRec :: (Integral a) => a -> a
 fibRec 0 = 0
 fibRec 1 = 1
 fibRec a | a > 0 = fibRec (a-2) + fibRec (a-1)
          | otherwise = error "Número negativo"
 
+-- Utiliza uma lista de tamanho finito para o cálculo de números de Fibonacci: a função map é utilizada em conjunto com uma
+-- função auxiliar (para as somas) que é definida dentro de fibLista - esta é aplicada sobre um intervalo de 2 até à ordem
+-- desejada, impedindo cálculos desnecessários.
 fibLista :: (Integral a) => a -> a
 fibLista n = last lista
   where lista = 0 : 1 : map foo [2..(fromIntegral n)]
         foo n = lista !! (n-1) + lista !! (n-2)
 
+
+-- Utiliza uma lista infinita para o cálculo de números de Fibonacci.
+-- Usa-se indexação para se obter a ordem pretendida e zipWith para as somas.
 fibListaInfinita :: (Integral a) => a -> a
 fibListaInfinita a = lista !! (fromIntegral a)
   where lista = 0 : 1 : zipWith (+) lista (drop 1 lista)
 
 -- Alínea 3
+-- Segue a mesma lógica que a correspondente da alínea 1,
+-- apenas usa as funções do módulo BigNumber para tal.
 fibRecBN :: BigNumber -> BigNumber
 fibRecBN [0] = [0]
 fibRecBN [1] = [1]
@@ -27,13 +36,18 @@ fibRecBN a | not (less a zero) = somaBN (fibRecBN (subBN a two)) (fibRecBN (subB
         one = [1]
         two = [2]
 
+-- Função auxiliar que vai recursivamente preenchendo uma lista
+-- com números de Fibonacci até à ordem n.
 fibListaBNAux:: BigNumber -> BigNumber -> BigNumber -> BigNumber
 fibListaBNAux [0] fib0 fib1 = fib0
 fibListaBNAux n fib0 fib1 = fibListaBNAux (subBN n [1]) fib1 (somaBN fib0 fib1) 
 
+-- Usa uma lista finita para o cálculo de números de Fibonacci com o módulo BigNumber. 
 fibListaBN :: BigNumber -> BigNumber
 fibListaBN n = fibListaBNAux n [0] [1]
 
+-- Segue a mesma lógica que a correspondente da alínea 1,
+-- apenas usa as funções do módulo BigNumber para tal.
 fibListaInfinitaBN :: BigNumber -> BigNumber
 fibListaInfinitaBN n = head (drop (fromBN n) lista)
   where lista = [zero] ++ [one] ++ [somaBN a b | (a, b)<- zip lista (tail lista)]
@@ -42,6 +56,8 @@ fibListaInfinitaBN n = head (drop (fromBN n) lista)
         two = [2]
 
 -- Estas funções só foram elaboradas no contexto da Alínea 4
+-- Seguem a mesma lógica das funções da alínea 1, apenas foi mudado o tipo,
+-- tendo-se em conta alterações necessárias através de fromIntegral.
 fibRecInteger :: Integer -> Integer
 fibRecInteger 0 = 0
 fibRecInteger 1 = 1
